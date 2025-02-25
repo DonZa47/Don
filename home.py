18bdaf97f0c26a2bd60480b75c5275f3beaa4b56
DonZa47 <nudbun73@gmail.com>
2025-02-25 11:35:55 +0700

    .

--- home.py
+++ home.py
@@ -7,15 +7,24 @@ st.header("🍖🍖Website Developing using Python🍖🍖")
 st.image('./img/pic.jpg')
 st.subheader("ธนดล จันทวงค์")
 col1,col2,col3 = st.columns(3)
+html_7 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_7, unsafe_allow_html=True)
+st.markdown("")
+dt = pd.read_csv("./data/iris-3.csv")
+st.write(dt.head(10))
 
 with col1:
     st.header("tuilpa")
-    st.imge("./img/fo1.jpg")
+    st.image("./img/fo1.jpg")
 
 with col2:
     st.header("Helianthus annuus")
-    st.imge("./img/fo2.jpg")
+    st.image("./img/fo2.jpg")
 
 with col3:
     st.header("Myostis")
-    st.imge("./img/fo3.jpg")
+    st.image("./img/fo3.jpg")
