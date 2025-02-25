ba56a23673bcfbde2f5cd67960e1d5a7d27faada
DonZa47 <nudbun73@gmail.com>
2025-02-25 11:26:27 +0700

    .

--- home.py
+++ home.py
@@ -6,3 +6,16 @@ st.header("🍖🍖Website Developing using Python🍖🍖")
 
 st.image('./img/pic.jpg')
 st.subheader("ธนดล จันทวงค์")
+col1,col2,col3 = st.columns(3)
+
+with col1:
+    st.header("tuilpa")
+    st.imge("./img/nim1.jpg")
+
+with col2:
+    st.header("Helianthus annuus")
+    st.imge("./img/nim2.jpg")
+
+with col3:
+    st.header("Myostis")
+    st.imge("./img/nim3.jpg")
